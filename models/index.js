d7c4340f0bac52789231fde7e75639c4f280957e
pripri99 <36493403+pripri99@users.noreply.github.com>
2022-01-22 16:49:20 -0500

    Connected database, create user and display users

--- models/index.js
+++ models/index.js
@@ -7,24 +7,25 @@ const fs = require('fs');
 
 const path = require("path");
  
+var Sequelize = require('sequelize-cockroachdb');
 // Connect to CockroachDB through Sequelize
 var sequelize = new Sequelize({
-  dialect: "postgres",
-  username: "pripri99",
-  password: "YqmXHAU05AOukdNdnSKfyg",
-  host: "free-tier11.gcp-us-east1.cockroachlabs.cloud",
-  port: 26257,
-  database: "scared-to-compile-18.defaultdb",
-  dialectOptions: {
-    ssl: {
-      
-      //For secure connection:
-      ca: fs.readFileSync(path.resolve(__dirname, "../certs/root.crt"))
-              .toString()
+    dialect: "postgres",
+    username: "pripri99",
+    password: "YqmXHAU05AOukdNdnSKfyg",
+    host: "free-tier11.gcp-us-east1.cockroachlabs.cloud",
+    port: 26257,
+    database: "scared-to-compile-18.defaultdb",
+    dialectOptions: {
+      ssl: {
+        
+        //For secure connection:
+        ca: fs.readFileSync(path.resolve(__dirname, "../certs/root.crt"))
+                .toString()
+      },
     },
-  },
-  logging: false, 
-});
+    logging: false, 
+  });
 
 var DataTypes = Sequelize.DataTypes;
 
@@ -32,61 +33,18 @@ if (!Sequelize.supportsCockroachDB) {
   throw new Error("CockroachDB dialect for Sequelize not installed");
 }
 
-module.exports.Customer = sequelize.define('customer', {
-  name: DataTypes.STRING
-}, {
-  timestamps: false
-});
-
-module.exports.customerToJSON = function(customer) {
-  return {
-    id: parseInt(customer.id),
-    name: customer.name
-  };
-};
-
-module.exports.Order = sequelize.define('order', {
-  customer_id: DataTypes.INTEGER,
-  subtotal: DataTypes.DECIMAL(18, 2)
-}, {
-  timestamps: false
-});
-
-module.exports.orderToJSON = function(order) {
-  return{
-    id: parseInt(order.id),
-    subtotal: order.subtotal,
-    customer: {
-      id: order.customer_id
-    }
-  };
-};
-
-module.exports.OrderProduct = sequelize.define('order_products', {
-  order_id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true
-  },
-  product_id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true
-  }
-}, {
-  timestamps: false
-});
-
-module.exports.Product = sequelize.define('product', {
+module.exports.User = sequelize.define('User', {
   name: DataTypes.STRING,
-  price: DataTypes.DECIMAL(18, 2)
+  time: DataTypes.STRING
 }, {
   timestamps: false
 });
 
-module.exports.productToJSON = function(product) {
+module.exports.userToJSON = function(user) {
+  //console.log(user)
   return {
-    id: parseInt(product.id),
-    name: product.name,
-    price: product.price,
+    name: user.name,
+    time: user.time
   };
 };
 
---
+++ routes/root.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+//const Client = require('../models/client');
+
+const db = require('../models/index');
+const User = db["User"];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  //res.type("text/plain");
+  //res.send('Node.js Sequelize test server');
+  res.render('pages/homepage');
+});
+
+
+router.get('/createUser', function(req, res, next) {
+  //res.type("text/plain");
+  //res.send('Node.js Sequelize test server');
+  res.render('pages/createUser');
+});
+
+router.get('/ping', function (req, res, next) {
+  res.type("text/plain");
+  res.send('node/sequelize');
+});
+
+router.get('/userList', async (req, res, next) => {
+  //res.type("text/plain");
+  //res.send('Node.js Sequelize test server');
+  try {
+
+    const users = await User.findAll();
+    console.log(users.every(user => user instanceof User)); // true
+    console.log("All client:", JSON.stringify(users, null, 2));
+    res.render('pages/displayUser', { users });
+  } catch (e) {
+    next(e);
+  }
+});
+module.exports = router;
