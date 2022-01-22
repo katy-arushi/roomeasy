e1f145ffb8edb1d8a0cf83f3986f561e3e029e50
Arushi Katyal <katy.arushi@gmail.com>
2022-01-21 23:32:04 -0500

    add ejs

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const logger = require("morgan");
 
 const port = process.env.PORT || 3001;
 
+app.set("view engine", "ejs");
 app.use(logger('dev'));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
