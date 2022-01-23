f30429a4b63ecf38d3751ef879dd696920a08546
Ilikedietpepsi <jarvishe210@gmail.com>
2022-01-23 00:05:31 -0500

    added more quiz questions

--- index.js
+++ index.js
@@ -45,4 +45,28 @@ app.get('/q3', function(req, res) {
   res.render('q3');
 });
 
+app.get('/q4', function(req, res) {
+  res.render('q4');
+});
+
+app.get('/q5', function(req, res) {
+  res.render('q5');
+});
+
+app.get('/q6', function(req, res) {
+  res.render('q6');
+});
+
+app.get('/q7', function(req, res) {
+  res.render('q7');
+});
+
+app.get('/q8', function(req, res) {
+  res.render('q8');
+});
+
+app.get('/q9', function(req, res) {
+  res.render('q9');
+});
+
 module.exports = app;
